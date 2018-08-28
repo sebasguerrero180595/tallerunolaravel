6faadd37e092b274561480e5f0015aa1634b7b7d
jonmid <jonmid@gmail.com>
2018-08-27 19:33:31 -0500

    first commit

---
+++ resources/views/layouts/plantilla2.blade.php
@@ -0,0 +1,27 @@
+<!doctype html>
+<html lang="{{ app()->getLocale() }}">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>TALLER UNO - #####</title>
+    </head>
+    <body>
+
+        <center>
+        <table border=1>
+            <tr>
+                <td colspan=2><center><b>#####</b></center></td>
+            </tr>
+
+            ###############
+
+            <tr>
+                <td colspan=2><center><b>#####</b></center></td>
+            </tr>
+        </table>
+        </center>
+        
+    </body>
+</html>
